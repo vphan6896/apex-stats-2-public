4dac86c17c733546dc8ebe44a981b35aaeb31be7
Elias Neypes <neypeseav_2020@utexas.edu>
2019-05-07 19:35:44 +0000

    unit test added

--- tests/clienttest.py
+++ tests/clienttest.py
@@ -1,4 +1,38 @@
+#!/usr/bin/python
+import os
+
+path = "/usr/tmp"
+
+# Check current working directory.
+retval = os.getcwd()
+print "Current working directory %s" % retval
+os.chdir('../app.py')
+# Import our app.py
+from app import search
+os.chdir(retval)
+
 import unittest
+import json
+from unittest.mock import MagicMock
+
+# mock impl from Johannes Fahrenkrug on Stack Overflow
+# This method will be used by the mock to replace requests.get
+def mocked_requests_get(*args, **kwargs):
+    class MockResponse:
+        def __init__(self, json_data, status_code):
+            self.json_data = json_data
+            self.status_code = status_code
+
+        def json(self):
+            return self.json_data
+
+    if args[0] == 'http://someurl.com/test.json':
+        return MockResponse({"key1": "value1"}, 200)
+    elif args[0] == 'http://someotherurl.com/anothertest.json':
+        return MockResponse({"key2": "value2"}, 200)
+    elif args[0] == 'https://api.github.com':
+        return MockResponse({"class": "cloud"}, 200)
+    return MockResponse(None, 404)
 
 
 class Testing(unittest.TestCase):
@@ -11,6 +45,12 @@ class Testing(unittest.TestCase):
         a = True
         b = True
         self.assertEqual(a, b)
+
+    @mock.patch('requests.get', side_effect=mocked_requests_get)
+    def test_search(self):
+	ret = app.search()
+	val = ret['class']
+	self.assertEqual(val, 'cloud')
 
 if __name__ == '__main__':
     unittest.main()
